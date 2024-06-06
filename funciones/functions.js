1736d08052004d11ce54bc9b527c5eacb8c637f8
Artoriasdev <fmontenegro2207@gmail.com>
2024-06-06 12:32:35 -0500

    first commit

---
+++ funciones/functions.js
@@ -0,0 +1,11 @@
+"use strict";
+(function () {
+    var hero = "Flash";
+    function returnName() {
+        return hero;
+    }
+    var activateBatisignal = function () {
+        return "Batisenal activada!";
+    };
+    console.log(typeof activateBatisignal);
+})();
---
+++ tipos/void.js
@@ -0,0 +1,13 @@
+"use strict";
+(function () {
+    function callBatman() {
+        return;
+    }
+    var callSuperman = function () {
+        return;
+    };
+    var a = callBatman();
+    console.log(a);
+    var b = callSuperman();
+    console.log(b);
+})();
